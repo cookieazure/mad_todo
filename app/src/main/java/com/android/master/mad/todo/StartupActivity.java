d2ea103e1a21eef045efaa6d4fcc7e8d2d07f94d
CookieAzure <timo.missler@gmail.com>
2016-05-27 09:55:19 +0200

    Initial commit.

---
+++ app/src/main/java/com/android/master/mad/todo/StartupActivity.java
@@ -0,0 +1,12 @@
+package com.android.master.mad.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
