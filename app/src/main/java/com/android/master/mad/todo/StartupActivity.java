4b28a7d1d9d0b84bf625ff3dd51419a8687bc145
CookieAzure <timo.missler@gmail.com>
2016-05-28 21:30:00 +0200

    Add splash screen.

--- app/src/main/java/com/android/master/mad/todo/StartupActivity.java
+++ app/src/main/java/com/android/master/mad/todo/StartupActivity.java
@@ -1,12 +1,21 @@
 package com.android.master.mad.todo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+/**
+ * Startup activity showing a simple splash screen and checking connection to web interface.
+ */
 public class StartupActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = StartupActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Splash screen initialized.");
+
+
     }
 }
