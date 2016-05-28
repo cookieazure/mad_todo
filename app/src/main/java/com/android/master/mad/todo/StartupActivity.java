651e95cc07108c78f40e7ca7d19443baa5f91b40
CookieAzure <timo.missler@gmail.com>
2016-05-28 22:44:33 +0200

    Prepare availability check for web interface .

--- app/src/main/java/com/android/master/mad/todo/StartupActivity.java
+++ app/src/main/java/com/android/master/mad/todo/StartupActivity.java
@@ -3,6 +3,7 @@ package com.android.master.mad.todo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Startup activity showing a simple splash screen and checking connection to web interface.
@@ -16,6 +17,21 @@ public class StartupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.d(LOG_TAG, "Splash screen initialized.");
 
+        if(isWebInterfaceAvailable()){
+            Log.i(LOG_TAG, "Web interface available.");
+        } else{
+            Log.i(LOG_TAG, "Web interface not available.");
+            Toast.makeText(StartupActivity.this, "The web interface is not available. Only local storage is used.", Toast.LENGTH_LONG).show();
+        }
+    }
 
+    private boolean isWebInterfaceAvailable(){
+        Log.d(LOG_TAG, "Run isWebInterfaceAvailable().");
+        checkWebInterface();
+        return false;
+    }
+
+    private void checkWebInterface(){
+        Log.d(LOG_TAG, "Run checkWebInterface().");
     }
 }
