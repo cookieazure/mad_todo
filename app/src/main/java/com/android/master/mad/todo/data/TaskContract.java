9e6ee3121dc78d5a8c6bab309b69609f9b7c29f5
CookieAzure <timo.missler@gmail.com>
2016-05-29 03:41:38 +0200

    Implement SQLite database for tasks.

---
+++ app/src/main/java/com/android/master/mad/todo/data/TaskContract.java
@@ -0,0 +1,60 @@
+package com.android.master.mad.todo.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Cookie on 29.05.2016.#
+ * Defines table and column name for the SQLite database.
+ */
+public class TaskContract{
+
+
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.android.master.mad.todo";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    public static final String PATH_TASKS = "tasks";
+
+    public static final class Task implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TASKS;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TASKS;
+
+        public static final String TABLE_NAME = "tasks";
+
+        // Name of the task
+        public static final String COLUMN_NAME = "name";
+        // Task description
+        public static final String COLUMN_DESC = "description";
+        // Task due date and time
+        public static final String COLUMN_DATE = "date";
+        // Indicator if task is finished
+        public static final String COLUMN_DONE = "done";
+        // Indicator if task is a priority
+        public static final String COLUMN_PRIO = "priority";
+        // Associated contacts for task
+        public static final String COLUMN_CONTACTS = "contacts";
+        // Associated location for task
+        public static final String COLUMN_LOC = "location";
+
+        public static Uri buildTaskUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/android/master/mad/todo/data/TaskDbHelper.java
@@ -0,0 +1,73 @@
+package com.android.master.mad.todo.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.android.master.mad.todo.data.TaskContract.Task;
+
+/**
+ * Created by Cookie on 29.05.2016.
+ * Database helper class.
+ */
+public class TaskDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "tasks.db";
+
+    public TaskDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a table to hold locations.  A location consists of the string supplied in the
+        // location setting, the city name, and the latitude and longitude
+        final String SQL_CREATE_TASKS_TABLE = "CREATE TABLE " + Task.TABLE_NAME + " (" +
+                Task._ID + " INTEGER PRIMARY KEY," +
+                Task.COLUMN_NAME + " TEXT UNIQUE NOT NULL, " +
+                Task.COLUMN_DESC + " TEXT, " +
+                Task.COLUMN_DATE + " INTEGER, " +
+                Task.COLUMN_DONE + " INTEGER NOT NULL, " +
+                Task.COLUMN_PRIO + " INTEGER NOT NULL " +
+                Task.COLUMN_CONTACTS + " TEXT, " +
+                Task.COLUMN_LOC + " TEXT " +
+                " );";
+
+        db.execSQL(SQL_CREATE_TASKS_TABLE);
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     * <p/>
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Hard database upgrade.
+        db.execSQL("DROP TABLE IF EXISTS " + Task.TABLE_NAME);
+        onCreate(db);
+    }
+}
