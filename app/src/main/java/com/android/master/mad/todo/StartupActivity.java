fbcc318242643229cd9d1b5b63b7400006c0139b
CookieAzure <timo.missler@gmail.com>
2016-05-31 17:00:44 +0200

    Add connectivity check.

--- app/src/main/java/com/android/master/mad/todo/StartupActivity.java
+++ app/src/main/java/com/android/master/mad/todo/StartupActivity.java
@@ -1,10 +1,19 @@
 package com.android.master.mad.todo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Cookie on 28.05.2016.
@@ -21,30 +30,46 @@ public class StartupActivity extends AppCompatActivity {
 
         if(isWebInterfaceAvailable()){
             Log.i(LOG_TAG, "Web interface available.");
+            //Intent intent = new Intent(this,LoginActivity.class);
+            //startActivity(intent);
         } else{
             Log.i(LOG_TAG, "Web interface not available.");
             Toast.makeText(StartupActivity.this, "The web interface is not available. Only local storage is used.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, TaskActivity.class);
+            intent.putExtra("WebInterface", false);
+            startActivity(intent);
         }
-
-        try
-        {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        Intent intent = new Intent(this, TaskActivity.class);
-        startActivity(intent);
-
     }
 
     private boolean isWebInterfaceAvailable(){
         Log.d(LOG_TAG, "Run isWebInterfaceAvailable().");
-        checkWebInterface();
+
+        if(checkConnection()){
+            try {
+                URL url = new URL(getResources().getString(R.string.base_url));
+
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestProperty("User-Agent", "Android Application");
+                con.setRequestProperty("Connection", "close");
+                con.setConnectTimeout(5000);
+                con.connect();
+
+                if (con.getResponseCode() == 200) {
+                    Log.i(LOG_TAG, ": response code 200.");
+                    return true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
-    private void checkWebInterface(){
+    private boolean checkConnection(){
         Log.d(LOG_TAG, "Run checkWebInterface().");
+
+        ConnectivityManager conManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = conManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
