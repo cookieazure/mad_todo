1795f1e23631e22cd90470d5a4ab20a2d0431b46
CookieAzure <timo.missler@gmail.com>
2016-06-04 01:21:07 +0200

    Fix initial connection check using AsyncTask.

---
+++ app/src/main/java/com/android/master/mad/todo/IAsyncResponse.java
@@ -0,0 +1,8 @@
+package com.android.master.mad.todo;
+
+/**
+ * Created by Cookie on 04.06.2016.
+ */
+public interface IAsyncResponse {
+    void evaluateConnection(boolean result);
+}
--- app/src/main/java/com/android/master/mad/todo/StartupActivity.java
+++ app/src/main/java/com/android/master/mad/todo/StartupActivity.java
@@ -1,50 +1,95 @@
 package com.android.master.mad.todo;
 
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.CheckBox;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
  * Created by Cookie on 28.05.2016.
  * Startup activity showing a simple splash screen and checking connection to web interface.
  */
-public class StartupActivity extends AppCompatActivity {
+public class StartupActivity extends AppCompatActivity implements IAsyncResponse{
 
     private final String LOG_TAG = StartupActivity.class.getSimpleName();
+    private ProgressDialog connectionDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(LOG_TAG, "Splash screen initialized.");
+        checkWebInterfaceAvailability();
+    }
 
-        if(isWebInterfaceAvailable()){
+    public void evaluateConnection(boolean result){
+        Log.d(LOG_TAG, ": evaluateConnection().");
+        if(result){
             Log.i(LOG_TAG, "Web interface available.");
             //Intent intent = new Intent(this,LoginActivity.class);
             //startActivity(intent);
         } else{
             Log.i(LOG_TAG, "Web interface not available.");
             Toast.makeText(StartupActivity.this, "The web interface is not available. Only local storage is used.", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, TaskActivity.class);
-            intent.putExtra("WebInterface", false);
-            startActivity(intent);
+//            Intent intent Intent= new Intent(this, TaskActivity.class);
+//            intent.putExtra("WebInterface", false);
+//            startActivity(intent);
         }
     }
 
-    private boolean isWebInterfaceAvailable(){
-        Log.d(LOG_TAG, "Run isWebInterfaceAvailable().");
-
+    private void checkWebInterfaceAvailability(){
+        Log.d(LOG_TAG, ": checkWebInterfaceAvailability().");
         if(checkConnection()){
+            Log.i(LOG_TAG, "Connection is true, check server connection.");
+            connectionDialog = new ProgressDialog(this);
+            connectionDialog.setMessage("Checking server connection.");
+            connectionDialog.show();
+            WebServiceConnection task = new WebServiceConnection(this);
+            task.execute();
+        }
+    }
+
+    private boolean checkConnection(){
+        Log.d(LOG_TAG, ": checkConnection().");
+        ConnectivityManager conManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = conManager.getActiveNetworkInfo();
+        Log.d(LOG_TAG, "Connection is: " + (activeNetwork != null) + " : " + activeNetwork.isConnectedOrConnecting());
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    private class WebServiceConnection extends AsyncTask<Void, Void, Boolean> implements IAsyncResponse {
+
+        IAsyncResponse delegate;
+
+        public WebServiceConnection(IAsyncResponse delegate){
+            this.delegate = delegate;
+        }
+        @Override
+        public void evaluateConnection(boolean result) {
+            Log.d(LOG_TAG, ": evaluateConnection().");
+            delegate.evaluateConnection(result);
+        }
+
+        protected void onPreExecute(){
+            Log.d(LOG_TAG, ": onPreExecute().");
+        }
+        protected void onPostExecute(Boolean result) {
+            Log.d(LOG_TAG, ": onPostExecute().");
+            connectionDialog.hide();
+            evaluateConnection(result);
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Log.d(LOG_TAG, ": doInBackground().");
             try {
                 URL url = new URL(getResources().getString(R.string.base_url));
 
@@ -61,15 +106,9 @@ public class StartupActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            return false;
         }
-        return false;
-    }
 
-    private boolean checkConnection(){
-        Log.d(LOG_TAG, "Run checkWebInterface().");
 
-        ConnectivityManager conManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = conManager.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 }
