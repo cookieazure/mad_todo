93f839fdd8b4e9c53cdfb2a56b1f431c69148115
CookieAzure <timo.missler@gmail.com>
2016-06-05 15:13:31 +0200

    Implement user authentication service.

---
+++ app/src/main/java/com/android/master/mad/todo/data/User.java
@@ -0,0 +1,41 @@
+package com.android.master.mad.todo.data;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Cookie on 05.06.2016.
+ * Basic class for user data.
+ */
+public class User {
+
+    @SerializedName("mail")
+    private String mail;
+    @SerializedName("pwd")
+    private String pwd;
+
+    // Default constructor
+    public User(){
+
+    }
+
+    public User(String mail, String pwd) {
+        this.mail = mail;
+        this.pwd = pwd;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
--- app/src/main/java/com/android/master/mad/todo/sync/ITaskAuthOperations.java
+++ app/src/main/java/com/android/master/mad/todo/sync/ITaskAuthOperations.java
@@ -1,8 +1,15 @@
 package com.android.master.mad.todo.sync;
 
+import com.android.master.mad.todo.data.User;
+
+import retrofit2.http.PUT;
+
 /**
  * Created by Cookie on 04.06.2016.
  * Interface for authentication operations on web service.
  */
 public interface ITaskAuthOperations {
+
+    @PUT("auth")
+    public boolean authenticateUser(User user);
 }
