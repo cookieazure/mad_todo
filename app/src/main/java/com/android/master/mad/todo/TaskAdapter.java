71bf9c0198cb4fc5ed1c7528ca53a468086ef565
CookieAzure <timo.missler@gmail.com>
2016-06-06 15:51:26 +0200

    Update list view item representation (normal, done, expired).

--- app/src/main/java/com/android/master/mad/todo/TaskAdapter.java
+++ app/src/main/java/com/android/master/mad/todo/TaskAdapter.java
@@ -2,6 +2,8 @@ package com.android.master.mad.todo;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,10 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by misslert on 31.05.2016.
@@ -53,7 +59,31 @@ public class TaskAdapter extends CursorAdapter {
 
         viewHolder.done.setChecked(cursor.getInt(TaskListActivity.COL_TASK_DONE) != 0);
         viewHolder.name.setText(cursor.getString(TaskListActivity.COL_TASK_NAME));
-        viewHolder.date.setText(cursor.getString(TaskListActivity.COL_TASK_DATE));
+        long expiry = cursor.getLong(TaskListActivity.COL_TASK_DATE);
+        if(expiry == 0){
+            viewHolder.date.setText(null);
+        } else {
+            String date = new SimpleDateFormat(view.getResources().getString(R.string.simple_date_format)).format(new Date(expiry));
+            viewHolder.date.setText(date);
+        }
         viewHolder.fav.setChecked(cursor.getInt(TaskListActivity.COL_TASK_FAV) != 0);
+        if(viewHolder.done.isChecked()) {
+            viewHolder.name.setPaintFlags(viewHolder.name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
+            viewHolder.date.setPaintFlags(viewHolder.name.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
+            viewHolder.name.setTextColor(Color.GRAY);
+            viewHolder.date.setTextColor(Color.GRAY);
+        } else {
+            viewHolder.name.setPaintFlags(0);
+            viewHolder.date.setPaintFlags(0);
+            if(expiry != 0 && expiry < Calendar.getInstance().getTimeInMillis()){
+                viewHolder.name.setTextColor(Color.RED);
+                viewHolder.date.setTextColor(Color.RED);
+            } else {
+                viewHolder.name.setTextColor(Color.BLACK);
+                viewHolder.date.setTextColor(Color.BLACK);
+            }
+        }
+
+
     }
 }
